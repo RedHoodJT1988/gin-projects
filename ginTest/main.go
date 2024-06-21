663cdd3baf4ee9cc7a82c21f79faf79f3454a054
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2024-06-21 01:19:45 -0700

    updates

--- ginTest/main.go
+++ ginTest/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func (c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/", IndexHandler)
 	router.Run()
+}
+
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	FirstName     string   `xml:"firstName,attr"`
+	LastName      string   `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Grayson", LastName: "Reeves"})
 }
